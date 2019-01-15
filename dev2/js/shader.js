34978073e7e80ef1c843eca1e90b4cb362e8b22f
登山 元気 <burst.blaster@gmail.com>
2019-01-16 01:25:58 +0900

    update shader

---
+++ dev2/js/shader.js
@@ -0,0 +1,19 @@
+if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.')
+}
+
+AFRAME.registerShader('red-shader', {
+    vertexShader: [
+        'varying vec2 vUV;',
+        'void main(void) {',
+        '  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
+        '  vUV = uv;',
+        '}'
+    ].join('\n'),
+
+    fragmentShader: [
+        'void main(void) {',
+        '    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); //(Red, Green, Blue, Alpha)',
+        '}'
+    ].join('\n')
+})
